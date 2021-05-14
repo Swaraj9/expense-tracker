9e0a76acd12a2a433f1dbb11f59fac8220948738
Swaraj <swaraj.dusane90@gmail.com>
2021-05-14 17:50:18 +0530

    Created Project expense-tracker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [balance, setBalance] = useState(0);
+  const [amount, setAmount] = useState('');
+  const [name, setName] = useState('');
+  const [expenseList, setExpenseList] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className = "main-container">
+        <div className = "container">
+          <div className = "container">
+            <h3>Balance:</h3>
+            <div className = "balance" style = {{color: balance > 0 ? "rgb(124, 207, 0)" : "rgb(255, 68, 0)"}}>{balance}$</div>
+          </div>
+          <div className = "input-container">
+            <input placeholder = "Name..." value = {name} onChange = {e => setName(e.target.value)} type = "text"/>
+            <input placeholder = "Amount..." value = {amount} onChange = {e => setAmount(e.target.value)} type = "number"/>
+            <button onClick = {() => {
+              if(!name || !amount) return;
+              setBalance(balance + parseFloat(amount));
+              setExpenseList([...expenseList, {name, amount}]);
+              setName('');
+              setAmount('');
+            }}>Calculate</button>      
+          </div> 
+        </div>
+        <div className = "container">
+          <h3>History:</h3>
+          <ul className = "list">
+            {expenseList.map(expense => <li className = {expense.amount > 0 ? "positive-expense expense" : "negetive-expense expense"}>{`${expense.name} : ${expense.amount}$`}</li>).reverse()}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
